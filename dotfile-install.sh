606ecee5bbd6f2f47cec4e61068f96ccb16e2736
TheTrueZeroTwo <85081508+TheTrueZeroTwo@users.noreply.github.com>
2022-01-30 13:33:05 -0700

    Create dotfile-install.sh

---
+++ dotfile-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+shell=0
+
+if [$(echo $0) = '/bin/bash';
+  then
+    set -o shell 1
+    echo -e 'if [ -f ~/.bash_aliases ]; then\n  source ~/.bash_aliases\nfi\n' > /home/$USER/.bashrc
+elif [$(echo $0) = '/bin/zsh';
+  then
+    set -o shell 2
+    echo -e 'if [ -f ~/.bash_aliases ]; then\n  source ~/.bash_aliases\nfi\n' > /home/$USER/.zshrc
+fi
+
+if [shell=1];then
+  cat /home/$USER/.bashrc | grep ".bash_aliases" >/dev/null && echo "susess!" || && echo "this script has failed"
+elif [shell=2];then
+  cat /home/$USER/.zshrc | grep ".bash_aliases" >/dev/null && echo "susess!" || && echo "this script has failed"
+fi
